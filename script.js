994e6be8d711b3bc3fc41f0f03c8ad8dc18b21a0
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-05-16 14:49:56 +0200

    finish

--- script.js
+++ script.js
@@ -3,26 +3,35 @@ var latitud = document.getElementById("latitud");
 var longitud = document.getElementById("longitud");
 var city = document.getElementById("city");
 // declaracion de variables para renderizar 
-const divLongitude = document.getElementById("longitude");
-const divLatitude = document.getElementById("latitude");
+const divCity = document.getElementById("cityName");
+const divLonLat = document.getElementById("lon&lat");
 const divDatetime = document.getElementById("datetime");
 const divTemp = document.getElementById("temp");
 const divPrecip = document.getElementById("precip");
 const divUv = document.getElementById("uv");
+const divClouds = document.getElementById("clouds");
 const divDescription = document.getElementById("description");
+const divICon = document.getElementById("icon");
 let h1 = document.createElement("h1");
-let h2 = document.createElement("h2");
+let h3 = document.createElement("h3");
 let h31 = document.createElement("h3");
 let h32 = document.createElement("h3");
 let h33 = document.createElement("h3");
+let h34 = document.createElement("h3");
+let h35 = document.createElement("h3");
+let h36 = document.createElement("h3");
+let h41 = document.createElement("h4");
+let h42 = document.createElement("h4");
+let h43 = document.createElement("h4");
+let h44 = document.createElement("h4");
+let h45 = document.createElement("h4");
+let h46 = document.createElement("h4");
+let imgIcon = document.createElement("img");
 //toma de valor introducidos manualmente de las coordenadas 
 let responseCoords = searchByCoords.addEventListener("click", inputEvent => {
     inputEvent.preventDefault();
     let valueLatitud = latitud.value;
     let valueLongitud = longitud.value;
-    if (valueLatitud < 0){       //consultar valores minimos de latitud
-        return alert("Latitude has to be greater than or equal to 0");
-    }
     if (valueLatitud > 90){    //consultar valores maximo de latitud
         return alert("Latitude must be less than or equal to 90")
     }
@@ -31,10 +40,10 @@ let responseCoords = searchByCoords.addEventListener("click", inputEvent => {
     }
     console.log(valueLatitud);   // print valores 
     if (valueLongitud < 0){       //consultar valores minimos de longitud
-        return alert("Latitude has to be greater than or equal to 0");
+        return alert("Longitude has to be greater than or equal to 0");
     }
     if (valueLongitud > 180){     //consultar valores maximos de longitud
-        return alert("Length has to be greater than or equal to 180")
+        return alert("Longitude has to be less than or equal to 180")
     }
     console.log(valueLongitud);
     
@@ -61,10 +70,66 @@ var searchFunctionbByCoord = function (latitud,longitud){
         method: 'GET',
         redirect: 'follow'
     };
-    fetch(`https://api.weatherbit.io/v2.0/current?lat=${latitud}&lon=${longitud}&key=7014245f346848edbb5410140f8d441c&lang=es&units=M`, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    const functionGPS = async function (requestOptions) {
+        const response = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${latitud}&lon=${longitud}&key=7014245f346848edbb5410140f8d441c&lang=es&units=M`, requestOptions)
+        console.log(response.ok);
+        console.log(response.status);
+        const data = await response.json();
+        console.log(data);
+        const superData = data.data;
+        console.log(superData);
+        const cityName = superData[0].city_name;
+        console.log(cityName);
+        h1.textContent = `${cityName}`;
+        divCity.appendChild(h1);
+        const lon = superData[0].lon;
+        console.log(lon);
+        const lat = superData[0].lat;
+        console.log(lat);
+        h3.textContent = `Longitud = ${lon} Latitud = ${lat}`;
+        divLonLat.appendChild(h3);
+        const datetime = superData[0].datetime;
+        console.log(datetime);
+        h32.textContent = `Fecha y Tiempo`
+        divDatetime.appendChild(h32);
+        h42.textContent = `${datetime}`;
+        divDatetime.appendChild(h42);
+        const precip = superData[0].precip;
+        console.log(precip);
+        h31.textContent = `Precipitación`
+        divPrecip.appendChild(h31);
+        h41.textContent = `${precip}`;
+        divPrecip.appendChild(h41);
+        const uv = superData[0].uv;
+        console.log(uv);
+        h33.textContent = `Rayos UV`
+        divUv.appendChild(h33);
+        h43.textContent = `${uv}`;
+        divUv.appendChild(h43);
+        const clouds = superData[0].clouds;
+        console.log(clouds);
+        h34.textContent = `Nubes`
+        divClouds.appendChild(h34);
+        h44.textContent = `${uv}`;
+        divClouds.appendChild(h44);
+        const temp = superData[0].temp;
+        console.log(clouds);
+        h35.textContent = `Temperatura`
+        divTemp.appendChild(h35);
+        h45.textContent = `${uv} ºC`;
+        divTemp.appendChild(h45);
+        const weather = superData[0].weather;
+        console.log(weather);
+        const code = weather.code;
+        console.log(code);
+        const description = weather.description;
+        console.log(description);
+        const icon = weather.icon;
+        console.log(icon);
+        imgIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+        divICon.appendChild(imgIcon);
+    };
+    functionGPS(requestOptions);
 };
 // funcion con los valores por ciudad - llamada a la api y obtencion de datos
 var searchFunctionbByCity = function (valueCity){
@@ -72,10 +137,66 @@ var searchFunctionbByCity = function (valueCity){
         method: 'GET',
         redirect: 'follow'
     };
-    fetch(`https://api.weatherbit.io/v2.0/current?key=7014245f346848edbb5410140f8d441c&lang=es&units=M&city=${valueCity}`, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    const functionGPS = async function (requestOptions) {
+        const response = await fetch(`https://api.weatherbit.io/v2.0/current?key=7014245f346848edbb5410140f8d441c&lang=es&units=M&city=${valueCity}`, requestOptions)
+        console.log(response.ok);
+        console.log(response.status);
+        const data = await response.json();
+        console.log(data);
+        const superData = data.data;
+        console.log(superData);
+        const cityName = superData[0].city_name;
+        console.log(cityName);
+        h1.textContent = `${cityName}`;
+        divCity.appendChild(h1);
+        const lon = superData[0].lon;
+        console.log(lon);
+        const lat = superData[0].lat;
+        console.log(lat);
+        h3.textContent = `Longitud = ${lon} Latitud = ${lat}`;
+        divLonLat.appendChild(h3);
+        const datetime = superData[0].datetime;
+        console.log(datetime);
+        h32.textContent = `Fecha y Tiempo`
+        divDatetime.appendChild(h32);
+        h42.textContent = `${datetime}`;
+        divDatetime.appendChild(h42);
+        const precip = superData[0].precip;
+        console.log(precip);
+        h31.textContent = `Precipitación`
+        divPrecip.appendChild(h31);
+        h41.textContent = `${precip}`;
+        divPrecip.appendChild(h41);
+        const uv = superData[0].uv;
+        console.log(uv);
+        h33.textContent = `Rayos UV`
+        divUv.appendChild(h33);
+        h43.textContent = `${uv}`;
+        divUv.appendChild(h43);
+        const clouds = superData[0].clouds;
+        console.log(clouds);
+        h34.textContent = `Nubes`
+        divClouds.appendChild(h34);
+        h44.textContent = `${uv}`;
+        divClouds.appendChild(h44);
+        const temp = superData[0].temp;
+        console.log(temp);
+        h35.textContent = `Temperatura`
+        divTemp.appendChild(h35);
+        h45.textContent = `${temp} ºC`;
+        divTemp.appendChild(h45);
+        const weather = superData[0].weather;
+        console.log(weather);
+        const code = weather.code;
+        console.log(code);
+        const description = weather.description;
+        console.log(description);
+        const icon = weather.icon;
+        console.log(icon);
+        imgIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+        divICon.appendChild(imgIcon);
+    };
+    functionGPS(requestOptions);
 };
 //accionar la toma de datos de coordenadas por el navegador y llamada a la api
 let responseGPS = searchByGPS.addEventListener("click", inputEvent => {
@@ -97,18 +218,46 @@ let responseGPS = searchByGPS.addEventListener("click", inputEvent => {
             console.log(data);
             const superData = data.data;
             console.log(superData);
+            const cityName = superData[0].city_name;
+            console.log(cityName);
+            h1.textContent = `${cityName}`;
+            divCity.appendChild(h1);
             const lon = superData[0].lon;
             console.log(lon);
             const lat = superData[0].lat;
             console.log(lat);
-            const precip = superData[0].precip;
-            console.log(precip);
+            h3.textContent = `Longitud = ${lon} Latitud = ${lat}`;
+            divLonLat.appendChild(h3);
             const datetime = superData[0].datetime;
             console.log(datetime);
+            h32.textContent = `Fecha y Tiempo`
+            divDatetime.appendChild(h32);
+            h42.textContent = `${datetime}`;
+            divDatetime.appendChild(h42);
+            const precip = superData[0].precip;
+            console.log(precip);
+            h31.textContent = `Precipitación`
+            divPrecip.appendChild(h31);
+            h41.textContent = `${precip}`;
+            divPrecip.appendChild(h41);
             const uv = superData[0].uv;
             console.log(uv);
+            h33.textContent = `Rayos UV`
+            divUv.appendChild(h33);
+            h43.textContent = `${uv}`;
+            divUv.appendChild(h43);
             const clouds = superData[0].clouds;
             console.log(clouds);
+            h34.textContent = `Nubes`
+            divClouds.appendChild(h34);
+            h44.textContent = `${uv}`;
+            divClouds.appendChild(h44);
+            const temp = superData[0].temp;
+            console.log(clouds);
+            h35.textContent = `Temperatura`
+            divTemp.appendChild(h35);
+            h45.textContent = `${uv} ºC`;
+            divTemp.appendChild(h45);
             const weather = superData[0].weather;
             console.log(weather);
             const code = weather.code;
@@ -117,24 +266,20 @@ let responseGPS = searchByGPS.addEventListener("click", inputEvent => {
             console.log(description);
             const icon = weather.icon;
             console.log(icon);
+            imgIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+            divICon.appendChild(imgIcon);
         };
         functionGPS(requestOptions);
-        // h1.textContent = `${result.data}`;
-        // basicsFeaturesOfPokemon.appendChild(h1);
-        // h3.textContent = `Basics Features`;
-        // basicsFeaturesOfPokemon.appendChild(h3);
-        // paragraph.textContent = `Weight:${pokemonWeight} Height:${pokemonHeight}`;
-        // basicsFeaturesOfPokemon.appendChilid(paragraph);
-        // h32.textContent = `Genetic`;
-        // geneticsOfPokemon.appendChild(h32);
-        // paragraph2.textContent = `Specie:${pokemonSpecies} Type:${typesArray}`;
-        // geneticsOfPokemon.appendChild(paragraph2);
-        // h33.textContent = `Abilities`;
-        // abilitiesOfPokemon.appendChild(h33);
-        // paragraph3.textContent = `Abilities ${abilitiesArray}`;
-        // abilitiesOfPokemon.appendChild(paragraph3);
     })
 })
+
+
+
+
+
+
+
+
 
 // var searchFunctionbByCoord = async function (values){
 //     const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${valueInput}`);
